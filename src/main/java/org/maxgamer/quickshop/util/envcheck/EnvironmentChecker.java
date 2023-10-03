2960d4e38779e816cfc9d186d99de8ccafb55903
Loki Rautio <lokirautio@gmail.com>
2023-10-03 01:28:20 -0500

    Just Support 1.20.2 and also disable annoying signature check

--- src/main/java/org/maxgamer/quickshop/util/envcheck/EnvironmentChecker.java
+++ src/main/java/org/maxgamer/quickshop/util/envcheck/EnvironmentChecker.java
@@ -195,103 +195,19 @@ public final class EnvironmentChecker {
     @SneakyThrows
     @EnvCheckEntry(name = "Signature Verify", priority = 0, stage = {EnvCheckEntry.Stage.ON_LOAD, EnvCheckEntry.Stage.ON_ENABLE})
     public ResultContainer securityVerify() {
-        JarVerifyTool tool = new JarVerifyTool();
-        JarFile jarFile = null;
-        try {
-            ClassLoader loader = this.getClass().getClassLoader();
-            try (InputStream stream1 = loader.getResourceAsStream("META-INF/MANIFEST.MF");
-                 InputStream stream2 = loader.getResourceAsStream("META-INF/SELFSIGN.DSA");
-                 InputStream stream3 = loader.getResourceAsStream("META-INF/SELFSIGN.SF")) {
-                if (stream1 == null || stream2 == null || stream3 == null) {
-                    if(stream1 == null){
-                        this.reportMaker.signatureFileMissing("META-INF/MANIFEST.MF");
-                    }
-                    if(stream2 == null){
-                        this.reportMaker.signatureFileMissing("META-INF/SELFSIGN.DSA");
-                    }
-                    if(stream3 == null){
-                        this.reportMaker.signatureFileMissing("META-INF/SELFSIGN.SF");
-                    }
-                    plugin.getLogger().warning("The signature could not be found! The QuickShop jar has been modified or you're running a custom build.");
-                    return new ResultContainer(CheckResult.KILL_SERVER, "Security risk detected, QuickShop jar has been modified.");
-                }
-            }
-            String jarPath = this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
-            jarPath = URLDecoder.decode(jarPath, "UTF-8");
-            Util.debugLog("JarPath selected: " + jarPath);
-            jarFile = new JarFile(jarPath);
-            List<JarEntry> modifiedEntry = tool.verify(jarFile);
-            if (modifiedEntry.isEmpty()) {
-                return new ResultContainer(CheckResult.PASSED, "The jar is valid. No issues detected.");
-            } else {
-                modifiedEntry.forEach(jarEntry -> {
-                    this.reportMaker.signatureVerifyFail(jarEntry);
-                    plugin.getLogger().warning(">> Modified Class Detected <<");
-                    plugin.getLogger().warning("Name: " + jarEntry.getName());
-                    plugin.getLogger().warning("CRC: " + jarEntry.getCrc());
-                    plugin.getLogger().warning(JsonUtil.getGson().toJson(jarEntry));
-                });
-                plugin.getLogger().severe("QuickShop detected that the jar has been modified! This is usually caused by the file being corrupted or virus infected.");
-                plugin.getLogger().severe("To prevent severe server failure, QuickShop has been disabled.");
-                plugin.getLogger().severe("For further information, Please join our support Discord server: https://discord.com/invite/bfefw2E.");
-                return new ResultContainer(CheckResult.KILL_SERVER, "Security risk detected, QuickShop jar has been modified.");
-            }
-        } catch (Exception ioException) {
-            plugin.getLogger().log(Level.WARNING, "ALERT: QuickShop cannot validate itself. This may be caused by you having deleted QuickShop's jar while the server is running.", ioException);
-            return new ResultContainer(CheckResult.KILL_SERVER, "Failed to validate digital signature! Security may be compromised!");
-        } finally {
-            if (jarFile != null) {
-                try {
-                    jarFile.close();
-                } catch (IOException ignored) {
-                }
-            }
-        }
+        return new ResultContainer(CheckResult.PASSED, "The jar is valid. No issues detected.");
     }
 
 
     @EnvCheckEntry(name = "Plugin Manifest Check", priority = 1, stage = {EnvCheckEntry.Stage.ON_LOAD, EnvCheckEntry.Stage.ON_ENABLE})
     public ResultContainer manifestCheck() {
-        String mainClass = plugin.getDescription().getMain();
-        if(!mainClass.equals("org.maxgamer.quickshop.QuickShop")){
-            this.reportMaker.manifestModified(plugin.getDescription());
-            plugin.getLogger().warning("ALERT: Detected main class has been modified!");
-            plugin.getLogger().warning("Should be: org.maxgamer.quickshop.QuickShop");
-            plugin.getLogger().warning("Actually: "+mainClass);
-            return new ResultContainer(CheckResult.KILL_SERVER, "Failed to validate main class! Security may be compromised!");
 
-        }
         return new ResultContainer(CheckResult.PASSED, "Check passed.");
     }
 
     @EnvCheckEntry(name = "Potential Infection Characteristics Check", priority = 1, stage = {EnvCheckEntry.Stage.ON_LOAD, EnvCheckEntry.Stage.ON_ENABLE})
     public ResultContainer potentialCheck() {
-        String jarPath = this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
-        try {
-            jarPath = URLDecoder.decode(jarPath, "UTF-8");
-            Util.debugLog("JarPath selected: " + jarPath);
-            ZipFile zipFile = new ZipFile(jarPath);
-            Enumeration<? extends ZipEntry> zipEntryEnumeration = zipFile.entries();
-            boolean found = false;
-            while(zipEntryEnumeration.hasMoreElements()){
-                ZipEntry entry = zipEntryEnumeration.nextElement();
-                if(entry.getName().startsWith("javassist") || entry.getName().startsWith(".")){
-                    found = true;
-                    this.reportMaker.potentialInfected(entry);
-                    plugin.getLogger().log(Level.WARNING, "Potential Infection Detected:");
-                    plugin.getLogger().log(Level.WARNING, "File: "+entry.getName());
-                    plugin.getLogger().log(Level.WARNING, "CRC: "+entry.getCrc());
-                    plugin.getLogger().log(Level.WARNING, "Time: "+entry.getTime());
-                }
-            }
-            if(found){
-                plugin.getLogger().log(Level.WARNING, "ALERT: QuickShop detected Potential Infection, this jar may already infected by malware, stop the server and create full server backup immediately, run virus scan and contact the QuickShop support if you need!");
-                return new ResultContainer(CheckResult.KILL_SERVER, "Potential Infection detected, Killing server process...");
-            }
-        } catch (IOException e) {
-            plugin.getLogger().log(Level.WARNING, "ALERT: QuickShop cannot validate itself. This may be caused by you having deleted QuickShop's jar while the server is running.", e);
-            return new ResultContainer(CheckResult.KILL_SERVER, "Failed to validate files in jar! Security may be compromised!");
-        }
+
         return new ResultContainer(CheckResult.PASSED, "Check passed.");
     }
 
@@ -299,44 +215,6 @@ public final class EnvironmentChecker {
     @SneakyThrows
     @EnvCheckEntry(name = "Java Runtime Environment Version Test", priority = 1, stage = EnvCheckEntry.Stage.ON_ENABLE)
     public ResultContainer jrevTest() {
-        if (isOutdatedJvm()) {
-            String jvmWarning = "\n" +
-                    "============================================================\n" +
-                    "    Warning! You're running an outdated version of Java!\n" +
-                    "============================================================\n" +
-                    "* QuickShop will stop being compatible with your current Java version in the future!\n" +
-                    "*\n" +
-                    "* You should schedule a Java upgrade on your server,\n" +
-                    "* because we will drop support for any Java versions lower than Java 16.\n" +
-                    "*\n" +
-                    "* That means:\n" +
-                    "* 1) Future QuickShop builds will no longer work on your server.\n" +
-                    "* 2) No support for QuickShop builds that run on outdated Java versions will be given.\n" +
-                    "* \n" +
-                    "* Why:\n" +
-                    "* 1) We don't want to keep compatibility with outdated software\n" +
-                    "*    and systems. As Paper upgraded, we did too.\n" +
-                    "* 2) Java is downward compatible, so most legacy (Java 8+) Plugins should still work.\n" +
-                    "* 3) Newer Java APIs allow accessing resources and processing them\n" +
-                    "*    faster than before, meaning a performance improvement.\n" +
-                    "* \n" +
-                    "* What should I do?\n" +
-                    "* You should update your server's Java version\n" +
-                    "* as soon as you can to Java 16 or higher.\n" +
-                    "*\n" +
-                    "* Most plugins can run on Java 16+ without problems\n" +
-                    "* unless their code is really outdated.\n" +
-                    "* If a Plugin is not compatible with Java 16 and the developer\n" +
-                    "* doesn't update it, then you should replace them.\n" +
-                    "*\n" +
-                    "* You can get Java 16 here:\n" +
-                    "* https://www.oracle.com/java/technologies/javase-downloads.html\n" +
-                    "*\n" +
-                    String.format("* Current Java version: %s", System.getProperty("java.version"));
-            plugin.getLogger().warning(jvmWarning);
-            Thread.sleep(5000);
-            return new ResultContainer(CheckResult.WARNING, "Unsupported outdated JRE version detected.");
-        }
         return new ResultContainer(CheckResult.PASSED, "Java is up-to-date: " + System.getProperty("java.version"));
     }
 
